1a2bdd629e14434edbe0255e08016f944c784de0
Nathaniel Benn <nrnbenn@gmail.com>
2026-05-24 07:15:04 +0100

    Added basic functions to get children in RootContainer and Component objects

--- Engine/internals/component.py
+++ Engine/internals/component.py
@@ -1,5 +1,6 @@
 class Component():
     def __init__(self, parent):
+        self.name = ""
         self.children = []
         self.parent = parent
 
@@ -18,3 +19,46 @@ class Component():
         pass
     def Tick(self):
         pass
+
+    def getChildByName(self, name, recursive=False): #returns first one it finds
+        result = self.getChildrenByName(name, recursive=recursive)
+        if len(result) == 0:
+            return(None)
+        return(result[0])
+
+    def getChildByType(self, type, recursive=False): #returns first one it finds
+        result = self.getChildrenByType(type, recursive=recursive)
+        if len(result) == 0:
+            return(None)
+        return(result[0])
+
+    def getChildrenByName(self, name, recursive=False): #returns all in a list
+        toSearch = self.children
+        results = []
+
+        while not len(toSearch) == 0:
+            search = toSearch.pop()
+            if search.name == name:
+                results.append(search)
+            if recursive:
+                newSearches = search.getAllChildren()
+                for newSearch in newSearches:
+                    toSearch.append(newSearch)
+
+        return(results)
+
+    def getChildrenByType(self, type, recursive=False): #returns all in a list
+        search = toSearch.pop()
+            if search.name == name:
+                results.append(search)
+            if recursive:
+                newSearches = search.getAllChildren()
+                for newSearch in newSearches:
+                    toSearch.append(newSearch)
+
+    def getAllChildren(self):
+        results = self.children
+        for child in results:
+            grandChildren = child.getAllChildren()
+            for grandchild in grandChildren:
+                results.append(grandchild)
--- Engine/internals/rootContainer.py
+++ Engine/internals/rootContainer.py
@@ -7,3 +7,47 @@ class RootContainer():
 
     def remove_child(self, child):
         self.children.remove(child)
+
+    def getChildByName(self, name, recursive=False): #returns first one it finds
+        result = self.getChildrenByName(name, recursive=recursive)
+        if len(result) == 0:
+            return(None)
+        return(result[0])
+
+    def getChildByType(self, type, recursive=False): #returns first one it finds
+        result = self.getChildrenByType(type, recursive=recursive)
+        if len(result) == 0:
+            return(None)
+        return(result[0])
+
+    def getChildrenByName(self, name, recursive=False): #returns all in a list
+        toSearch = self.children
+        results = []
+
+        while not len(toSearch) == 0:
+            search = toSearch.pop()
+            if search.name == name:
+                results.append(search)
+            if recursive:
+                newSearches = search.getAllChildren()
+                for newSearch in newSearches:
+                    toSearch.append(newSearch)
+
+        return(results)
+
+    def getChildrenByType(self, type, recursive=False): #returns all in a list
+        search = toSearch.pop()
+            if search.name == name:
+                results.append(search)
+            if recursive:
+                newSearches = search.getAllChildren()
+                for newSearch in newSearches:
+                    toSearch.append(newSearch)
+
+    def getAllChildren(self):
+        results = self.children
+        for child in results:
+            grandChildren = child.getAllChildren()
+            for grandchild in grandChildren:
+                results.append(grandchild)
+
